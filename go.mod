8e216fbc70e256a3d2fd90c60b234bab5350e4e4
sylvain <sylvain.cristofari@livingpackets.com>
2023-12-16 15:34:32 +0100

    change hid module

--- go.mod
+++ go.mod
@@ -1,6 +1,10 @@
 module github.com/kpeu3i/gods4
 
+go 1.21
+
 require (
-	github.com/pkg/errors v0.8.0
-	github.com/stamp/hid v0.0.0-20190105143849-bc55d7d13ce1
+	github.com/pkg/errors v0.9.1
+	github.com/sstallion/go-hid v0.14.1
 )
+
+require golang.org/x/sys v0.15.0 // indirect
--- hid/device.go
+++ hid/device.go
@@ -1,99 +1,103 @@
 package hid
 
 import (
-	"github.com/pkg/errors"
-	"github.com/stamp/hid"
+    "github.com/pkg/errors"
+    "github.com/sstallion/go-hid"
 )
 
 var (
-	vendorIDs  = [...]uint16{1356}
-	productIDs = [...]uint16{2508, 1476}
+    vendorIDs  = [...]uint16{1356}
+    productIDs = [...]uint16{2508, 1476}
 )
 
 type Device struct {
-	hidDeviceInfo *hid.DeviceInfo
-	hidDevice     *hid.Device
+    hidDeviceInfo *hid.DeviceInfo
+    hidDevice     *hid.Device
 }
 
 func (d *Device) VendorID() uint16 {
-	return d.hidDeviceInfo.VendorID
+    return d.hidDeviceInfo.VendorID
 }
 
 func (d *Device) ProductID() uint16 {
-	return d.hidDeviceInfo.ProductID
+    return d.hidDeviceInfo.ProductID
 }
 
 func (d *Device) Path() string {
-	return d.hidDeviceInfo.Path
+    return d.hidDeviceInfo.Path
 }
 
 func (d *Device) Release() uint16 {
-	return d.hidDeviceInfo.Release
+    return d.hidDeviceInfo.ReleaseNbr
 }
 
 func (d *Device) Serial() string {
-	return d.hidDeviceInfo.Serial
+    return d.hidDeviceInfo.SerialNbr
 }
 
 func (d *Device) Manufacturer() string {
-	return d.hidDeviceInfo.Manufacturer
+    return d.hidDeviceInfo.MfrStr
 }
 
 func (d *Device) Product() string {
-	return d.hidDeviceInfo.Product
+    return d.hidDeviceInfo.ProductStr
 }
 
 func (d *Device) Open() error {
-	hidDevice, err := d.hidDeviceInfo.Open()
-	if err != nil {
-		return err
-	}
+    hidDevice, err := hid.Open(d.VendorID(), d.ProductID(), d.Serial())
+    if err != nil {
+        return err
+    }
 
-	d.hidDevice = hidDevice
+    d.hidDevice = hidDevice
 
-	return nil
+    return nil
 }
 
 func (d *Device) Close() error {
-	return d.hidDevice.Close()
+    return d.hidDevice.Close()
 }
 
 func (d *Device) Read(b []byte) (int, error) {
-	return d.hidDevice.Read(b)
+    return d.hidDevice.Read(b)
 }
 
 func (d *Device) Write(b []byte) (int, error) {
-	return d.hidDevice.Write(b)
+    return d.hidDevice.Write(b)
 }
 
 func (d *Device) GetFeatureReport(code byte) ([]byte, error) {
-	var bytes []byte
+    var bytes []byte
 
-	switch code {
-	case 0x04:
-		bytes = make([]byte, 67)
-		bytes[0] = code
-		_, err := d.hidDevice.GetFeatureReport(bytes)
-		if err != nil {
-			return nil, err
-		}
-	default:
-		return nil, errors.Errorf("hid: unsupported report code: %v", code)
-	}
+    switch code {
+    case 0x04:
+        bytes = make([]byte, 67)
+        bytes[0] = code
+        _, err := d.hidDevice.GetFeatureReport(bytes)
+        if err != nil {
+            return nil, err
+        }
+    default:
+        return nil, errors.Errorf("hid: unsupported report code: %v", code)
+    }
 
-	return bytes, nil
+    return bytes, nil
 }
 
-func Find() []*Device {
-	var devices []*Device
+func Find() ([]*Device, error) {
+    var devices []*Device
 
-	for _, vendorID := range vendorIDs {
-		for _, productID := range productIDs {
-			for _, info := range hid.Enumerate(vendorID, productID) {
-				devices = append(devices, &Device{hidDeviceInfo: &info})
-			}
-		}
-	}
+    for _, vendorID := range vendorIDs {
+        for _, productID := range productIDs {
+            err := hid.Enumerate(vendorID, productID, func(info *hid.DeviceInfo) error {
+                devices = append(devices, &Device{hidDeviceInfo: info})
+                return nil
+            })
+            if err != nil {
+                return nil, err
+            }
+        }
+    }
 
-	return devices
+    return devices, nil
 }
